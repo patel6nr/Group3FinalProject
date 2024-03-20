7039251fa1e55533c44c4456cf35204c49b89ffb
ajwhite00 <aaronjwhite00@gmail.com>
2024-03-20 14:32:36 -0400

    Convert Project to Spring Boot

---
+++ src/main/java/com/bloggerhub/thebloggerhub/TheBloggerHubApplication.java
@@ -0,0 +1,13 @@
+package com.bloggerhub.thebloggerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBloggerHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheBloggerHubApplication.class, args);
+    }
+
+}
